eb6125f23a4725bea72badc70b27741dcbb4c6ce
Martinegeli <martinegeli9@gmail.com>
2020-10-24 13:38:21 +0200

    First MVP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+function getStatus() {
+
+}
+
 function App() {
+
+  let data = 0;
+  let status;
+  if (data) {
+    status = <div className="dahls">
+      <h4 style={{ color: "green" }}>Status: Mye dahls i skap</h4>
+      <div className="circles">
+        <img src="/green.png" alt="Lots of dahls" />
+        <img src="/red.png" alt="Empty" style={{ opacity: 0.2 }} />
+      </div>
+    </div>
+  } else {
+    status = <div className="tomt">
+      <h4 style={{ color: "red" }}>Helvette det er tomt, kontakt dagsfylla ASAP</h4>
+      <div className="circles">
+        <img src="/green.png" alt="Go dahls" style={{ opacity: 0.2 }} />
+        <img src="/red.png" alt="Empty" />
+      </div>
+    </div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <div className="header">
+        <h1>
+          Velkommen til <span style={{ color: 'red' }}>dahls</span>.<span style={{ color: 'green' }}>tech</span>
+        </h1>
+        <h4 style={{ fontStyle: "italic", paddingBottom: "20px" }}>
+          - En trust-løsning for de som liker å drikke iskald 0,33 dahls på sal
+          </h4>
+      </div>
+      <div className="body">
+        <div className="status">
+          {status}
+        </div>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Betaling: Skjer foreløpig over Vipps (se kjøleskap), men et mer avansert brukersystem er under utvikling!
         </p>
+
+        <p>
+          Lokasjon: Køddehjørnet mellom salplass A545 (Johannesen) og 5117 (Ankile)
+        </p>
+      </div>
+      <div className="footer">
+        Powered by dagsfylla
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.dagsfylla.no"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <img src="/dahls.png" alt="Dagsfylla" />
         </a>
-      </header>
+        <p style={{ paddingLeft: 10 }}>og vi som liker dahls på glass</p>
+        <a
+          href="https://www.ecdahls.no"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="/dahls.png" alt="Dahls på glass" />
+        </a>
+      </div>
     </div>
   );
 }
